9cc1a3a5f3f4eb10ede995fbbdd725adfd785d83
Cherkashin Alexander <cherkalexander@gmail.com>
2023-03-31 23:37:29 +0300

    Add ability to login with token

--- src/index.js
+++ src/index.js
@@ -21,6 +21,25 @@ async function initSwaggerUI() {
 
       return request;
     },
+    onComplete: () => {
+      const isAuthorized = !!localStorage.getItem('authorized');
+      
+      const tokenButton = document.createElement('button');
+      const className = isAuthorized ? 'locked' : 'unlocked'
+      tokenButton.className = `btn authorize ${className}`;
+      tokenButton.innerHTML = `<span>${(isAuthorized ? 'Authorized' : 'Login with Token')}</span><svg width="20" height="20"><use href="#${className}" xlink:href="#${className}"></use></svg>`
+
+      tokenButton.addEventListener('click', () => {
+        const token = prompt('Provide token');
+
+        if (token) {
+          localStorage.setItem("authorized", `{"oAuthProxied":{"schema":{"flow":"password","tokenUrl":"https://yandex-music-cors-proxy.onrender.com/https://oauth.yandex.ru/token","scopes":{},"type":"oauth2","description":"Authentication via proxy server to bypass cors"},"clientId":"23cabbbdc6cd418abb4b39c32c41195d","name":"oAuthProxied","appName":"Swagger UI Webpack Demo","passwordType":"basic","username":"<email>","token":{"access_token":"${token}","expires_in":30262901,"token_type":"bearer","uid":541320800},"clientSecret":"53bc75238f0c4d08a118e51fe9203300","password":"<password>","scopes":[]}}`);
+          localStorage.setItem("authorized", `{"oAuthProxied":{"schema":{"flow":"password","tokenUrl":"https://oauth.yandex.ru/token","scopes":{},"type":"oauth2","description":"Authentication via proxy server to bypass cors"},"clientId":"23cabbbdc6cd418abb4b39c32c41195d","name":"oAuthProxied","appName":"Swagger UI Webpack Demo","passwordType":"basic","username":"<email>","token":{"access_token":"${token}","expires_in":30262901,"token_type":"bearer","uid":541320800},"clientSecret":"53bc75238f0c4d08a118e51fe9203300","password":"<password>","scopes":[]}}`);
+          location.reload();
+        }
+      })
+      document.getElementsByClassName('auth-wrapper')[0].append(tokenButton);
+    }
   });
 
   ui.initOAuth({
